ab6ebebd4a432a7c4ec1b711e50eed6a7108c26e
Andrea Righi <an.righi7@gmail.com>
2023-12-04 09:34:56 +0100

    removed workflows

--- src/utility/Parser.php
+++ src/utility/Parser.php
@@ -6,13 +6,13 @@ class Parser
 {
     public static function parseInputWrapper(callable $callback)
     {
-        echo 'Started parsing input' . "\n";
+        //echo 'Started parsing input' . "\n";
         $startParsing = microtime(true);
 
         $lines = $callback();
 
         $endParsing = microtime(true);
-        echo 'Finished parsing input in ' . ($endParsing - $startParsing) . ' seconds' . "\n";
+        //echo 'Finished parsing input in ' . ($endParsing - $startParsing) . ' seconds' . "\n";
 
         return $lines;
     }
